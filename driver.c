ee652efe06bee0ff831ad870d3594b7298c1783d
brusnak89 <rhcp002743@gmail.com>
2017-03-30 14:52:00 -0500

    Update driver.c

--- driver.c
+++ driver.c
@@ -1,1 +1,13 @@
 
+int findCourse(Graph graph, char szCourseId[])
+{
+  int i;
+  for (i = 0; i < graph->iNumVertices; i++)
+  {
+    if (strcmp(szCourseId, graph->vertexM[i].szCourseId) == 0)
+    {
+      return i;
+    }
+  }
+  return NULL;
+}
