fbde4e19880df71c95ff833bba6628e5345ac37c
brusnak89 <rhcp002743@gmail.com>
2017-03-31 17:21:41 -0500

    update print.c
    work in progress need to figure out how prereqs and successors are handled

--- print.c
+++ print.c
@@ -1,16 +1,40 @@
 void printAllInList(Graph graph)
 {
+  int i;
+  for (i = 0; i < graph->iNumVertices; i++)
+  {
+    printf("%d %d %s %s %s %s"
+         ,i
+         ,0 //te
+         ,graph->vertexM[i].szCourseId
+         ,graph->vertexM[i].szCourseName
+         ,
 }
 /*********************************/
+/*******requires a findCourse call to pass in iVertex******
+iVertex = findCourse(graph, szCourseId - what was grabbed from sscanf)*********/
 void printOne(Graph graph, int iVertex)
 {
+  printf("%d %d %s %s %s %s"
+         ,iVertex
+         ,0 //te
+         ,graph->vertexM[iVertex].szCourseId
+         ,graph->vertexM[iVertex].szCourseName
+         ,graph->vertexM[iVertex]
 }
 /*********************************/
 void printSources(Graph graph)
 {
+  int i;
+  for (i = 0; i < graph->iNumVertices; i++)
+  {
+    if (vertexM[i]
 }
 /*********************************/
 void printSinks(Graph graph)
 {
+  int i;
+  for (i = 0; i < graph->iNumVertices; i++)
+  {
 }
 
