a48f7cd40bad23d463a61a892a8c951eeefe4cc7
Travis80 <woods49warhorse@hotmail.com>
2017-03-31 14:06:43 -0500

    Added getToken and main function

--- driver.c
+++ driver.c
@@ -5,6 +5,15 @@
 #include <stdarg.h>
 #include <stdlib.h>
 #include "cs2123p5.h"
+void main()
+{
+  char *pszCOMMAND;
+  
+  getToken(pszCOMMAND, )
+  char szInputBuffer[100];
+  while(fgets(szInputBuffer, 100, stdin) != NULL)
+}
+/************************************************/
 int findCourse(Graph graph, char szCourseId[])
 {
   int i;
@@ -16,4 +25,45 @@ int findCourse(Graph graph, char szCourseId[])
     }
   }
   return NULL;
+}
+/*************************************************/
+char * getToken(char *pszInputTxt, char szToken[], int iTokenSize)
+{
+    int iDelimPos;                      // found position of delim
+    int iCopy;                          // number of characters to copy
+    char szDelims[20] = " \n\r";        // delimiters
+    szToken[0] = '\0';
+
+    // check for NULL pointer 
+    if (pszInputTxt == NULL)
+        ErrExit(ERR_ALGORITHM
+        , "getToken passed a NULL pointer");
+
+    // Check for no token if at zero byte
+    if (*pszInputTxt == '\0')
+        return NULL;
+
+    // get the position of the first delim
+    iDelimPos = strcspn(pszInputTxt, szDelims);
+
+    // if the delim position is at the first character, return no token.
+    if (iDelimPos == 0)
+        return NULL;
+
+    // see if we have more characters than target token, if so, trunc
+    if (iDelimPos > iTokenSize)
+        iCopy = iTokenSize;             // truncated size
+    else
+        iCopy = iDelimPos;
+
+    // copy the token into the target token variable
+    memcpy(szToken, pszInputTxt, iCopy);
+    szToken[iCopy] = '\0';              // null terminate
+
+    // advance the position
+    pszInputTxt += iDelimPos;
+    if (*pszInputTxt == '\0')
+        return pszInputTxt;
+    else
+        return pszInputTxt + 1;
 }
