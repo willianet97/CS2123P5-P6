f592d20f85663b093e8614073b7aa858fa5b491a
Astlky <astl.kyle@yahoo.com>
2017-04-02 17:38:07 -0500

    Update insert.c

--- insert.c
+++ insert.c
@@ -2,3 +2,26 @@ void insertPrereq(Graph graph, int iPrereqVertex, int iCourseVertex)
 {
   
 }
+/************************** newGraph ********************************
+Graph newGraph()
+Purpose:
+    Creates a new graph.
+Parameters:
+    n/a
+Returns:
+    The new graph.
+Notes:
+    n/a
+**************************************************************************/
+Graph newGraph()
+{
+   // allocate memory for graph
+   Graph g = malloc(sizeof(GraphImp));
+   
+   // mark the graph as empty
+   g->iNumVertices = 0;
+  
+   // g->vertexM will be set in driver
+  
+   return g;
+}
