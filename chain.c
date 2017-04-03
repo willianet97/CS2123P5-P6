9066c5bedc6551003acd6e5ac2831d6ab5f6daac
Travis80 <woods49warhorse@hotmail.com>
2017-04-03 11:44:03 -0500

    Update chain.c

--- chain.c
+++ chain.c
@@ -1,7 +1,19 @@
 int maxChainLength(Graph graph, int iVertex)
 {
+  //still working on the basics of this one
+    EdgeNode *e;
+    int iCount = 0;
+    // traverse to the adjacent vertices
+    for (e = g->vertexM[iVertex].successorList; e != NULL; 
+         e = e->pNextEdge)
+    {
+        iCount += countG(g, e->iVertex);
+    }
+    return iCount;
 }
 
+}
+/***********************************************************/
 void printLongChains(Graph graph, int iVertex, int pathM[], int iLevel, int iLongLength)
 {
 }
