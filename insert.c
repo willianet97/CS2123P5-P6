5f35742a961981d2a00762ada1ec7dd0811727a5
Travis80 <woods49warhorse@hotmail.com>
2017-04-05 14:41:08 -0500

    Update insert.c

--- insert.c
+++ insert.c
@@ -1,3 +1,11 @@
+#define CRT SECURE NO WARNINGS 1
+//include files
+#include <stdio.h>
+#include <string.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include "cs2123p5.h"
+
 /***************************** causesCycle ********************************
 int causesCycle(Graph graph, int iPrereqVertex, int iVertex)
 Purpose:
