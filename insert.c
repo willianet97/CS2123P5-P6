8e167174eaad1cd38b80225626270b6b96148f7a
Astlky <astl.kyle@yahoo.com>
2017-04-05 15:43:32 -0500

    Update insert.c

--- insert.c
+++ insert.c
@@ -30,7 +30,7 @@ int causesCycle(Graph graph, int iPrereqVertex, int iVertex)
   EdgeNode *e; // EdgeNode for traversal
   for (e = graph->vertexM[iVertex].successorList; e != NULL; e = e->pNextEdge)
   {
-      causesCycle(graph, iPrereqVertex, graph->vertexM[iVertex].successorList->iSuccVertex);
+      // i don't think we need this : causesCycle(graph, iPrereqVertex, graph->vertexM[iVertex].successorList->iSuccVertex);
 
      if (graph->vertexM[iVertex].successorList->iSuccVertex == iPrereqVertex) // check if successors of iVertex reach iPrereqVertex
          return TRUE;                                                         // is this is the only case that causes cycle?
