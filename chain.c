0fbfa4fab960abe85120810c595586d7a9559a49
Travis80 <woods49warhorse@hotmail.com>
2017-04-13 16:04:51 -0500

    Update chain.c

--- chain.c
+++ chain.c
@@ -7,26 +7,17 @@
 #include "cs2123p5.h"
 int maxChain(Graph graph, int iVertex)
 {
-  if(iVertex <= 0)
-    return 0;
-  EdgeNode *e;// does this have to be malloced too? i'm suddenly forgetting the nitty gritty details.... no its a local pointer variable
-  int iCount = 0;
-  int iFirst = 0;
+  EdgeNode *e;
+  int iCount = 1;
   // traverse to the adjacent vertices
   //goes through successor lists
   for (e = graph->vertexM[iVertex].successorList; e != NULL; 
      e = e->pNextEdge)
   {
     //within successor lists it takes each path
-    iCount += maxChain(graph, e->iSuccVertex);
-    if(iCount >= iFirst)
-      iFirst = iCount;
-    //somehow this should be reset
-    iCount--;
-    //but no quite like this because if it has more
-    //than one successor it subracts too many
+    iCount = 1 + maxChain(graph, e->iSuccVertex);//this almost works except for cs1713 and further courses
   }
-  return iFirst;
+  return iCount;
 }
 /***********************************************************/
 /*maxchain is always called before this function
@@ -38,21 +29,20 @@ void printLongChains(Graph graph, int iVertex, int pathM[], int iLevel, int iLon
     return;
   EdgeNode *e;
   int i;
-  pathM[iLevel] = iVertex;// this reads current vertex into pathM
+  pathM[iLevel] = iVertex;// this doesn't update for another long chain for some reason, inside the forloop causes segfault
   for (e = graph->vertexM[iVertex].successorList; e!= NULL;
        e = e->pNextEdge)
   {
     printLongChains(graph, graph->vertexM[iVertex].successorList->iSuccVertex, pathM, iLevel+1, iLongLength);
     // we want to be able to overwrite shorter paths 
-    // without permanently increasing iLevel
-    if((iLevel+1) == iLongLength)
+  }
+  if((iLevel+1) == iLongLength)
+  {
+    printf("\tLongest chains beginning with %s\n", graph->vertexM[pathM[0]].szCourseId); // this will always be 0
+    for(i = 0; i < iLevel; i++)
     {
-      printf("\tLongest chains beginning with %s\n", graph->vertexM[pathM[0]].szCourseId); // this will always be 0
-      for(i = 0; i < iLevel; i++)
-      {
-        printf("%-8s", graph->vertexM[pathM[i]].szCourseId);
-      }
-      printf("%-8s\n", graph->vertexM[pathM[iLevel]].szCourseId);
+      printf("%-8s", graph->vertexM[pathM[i]].szCourseId);
     }
+    printf("%-8s\n", graph->vertexM[pathM[iLevel]].szCourseId);
   }
 }
