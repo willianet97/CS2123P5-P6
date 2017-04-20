efcfdb7d8d0b83a86f4cf12f1b41fc38f9912a46
Travis80 <woods49warhorse@hotmail.com>
2017-04-20 15:07:49 -0500

    Add files via upload

---
+++ Program6/doPlan.c
@@ -0,0 +1,14 @@
+#define CRT SECURE NO WARNINGS 1
+//include files
+#include <stdio.h>
+#include <string.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include "cs2123p5.h"
+/**************************** doPlan *************************************
+
+*************************************************************************/
+void doPlan(Graph graph, Plan plan)
+{
+
+}
