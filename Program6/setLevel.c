9867b5e54ef91c87bf1c1575c229d7bf628ed3bb
Travis80 <woods49warhorse@hotmail.com>
2017-04-21 18:43:54 -0500

    Not sure if this is right but working on it

--- Program6/setLevel.c
+++ Program6/setLevel.c
@@ -9,5 +9,28 @@
 ******************************************************************************/
 void setLevel(Graph graph, Plan plan, int iVertex, int iLev)
 {
-    //breadth first traversal
+    //iLev is going to be returned through parameter list so it is passed an address
+    //to an integer
+    //this function does not traverse. All it does is assign a specific semester
+    //to a course
+    EdgeNode *e;
+    iLev = 0;
+    for(e = graph->vertexM[iVertex].prereqList; e != NULL;
+       e = e->pNextEdge)
+    {
+        if(!(findPrereq(plan, iLev, iVertex)))
+            iLev++;
+    }
+}
+/*************************** findPrereq ***********************************
+**************************************************************************/
+int findPrereq(Plan plan, int iLev, int iPrereqVertex)
+{
+    int i;
+    for(i = 0; i < 5; i++)
+    {
+        if(iPrereqVertex == plan->semesterM[iLev][i])
+            return TRUE;
+    }
+    return FALSE;
 }
