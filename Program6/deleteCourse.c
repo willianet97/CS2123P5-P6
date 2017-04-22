560aa46400d16b9a35396270ac2b61464824a076
Astlky <astl.kyle@yahoo.com>
2017-04-22 18:47:43 -0500

    Update deleteCourse.c

--- Program6/deleteCourse.c
+++ Program6/deleteCourse.c
@@ -1,3 +1,11 @@
+#define CRT SECURE NO WARNINGS 1
+//include files
+#include <stdio.h>
+#include <string.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include "cs2123p6.h"
+
 /******************* deleteCourse **********************
 void deleteCourse(Graph graph, int iVertex)
 Purpose:
