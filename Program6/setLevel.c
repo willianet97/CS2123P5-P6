dbd0236cb16289e5ef324d85cfdb8183d79ce944
Travis80 <woods49warhorse@hotmail.com>
2017-04-24 17:36:41 -0500

    Update setLevel.c

--- Program6/setLevel.c
+++ Program6/setLevel.c
@@ -9,17 +9,24 @@
 ******************************************************************************/
 void setLevel(Graph graph, Plan plan, int iVertex, int iLev)
 {
-    //iLev is going to be returned through parameter list so it is passed an address
-    //to an integer
     //this function does not traverse. All it does is assign a specific semester
     //to a course
     EdgeNode *e;
     iLev = 0;
+    int i;
     for(e = graph->vertexM[iVertex].prereqList; e != NULL;
        e = e->pNextEdge)
     {
         if(!(findPrereq(plan, iLev, iVertex)))
             iLev++;
+    }
+    for(i = 0; i < 5; i++)
+    {
+        if(plan->semeserM[iLev][i] == -1)
+        {
+            plan->semesterM[iLev][i] = iVertex;//assign semester level
+            break;
+        }
     }
 }
 /*************************** findPrereq ***********************************
@@ -29,6 +36,10 @@ int findPrereq(Plan plan, int iLev, int iPrereqVertex)
     int i;
     for(i = 0; i < 5; i++)
     {
+        //searchs the semester plan for the iPrereqVertex of the current edgenode
+        //this compares to see if the course has already been put into a previous semester
+        //if it has been then it returns true
+        //if it hasn't been then it returns false
         if(iPrereqVertex == plan->semesterM[iLev][i])
             return TRUE;
     }
