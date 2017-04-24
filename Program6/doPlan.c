bc5392f177ab7ec43372492db52bb4f607abb28c
Travis80 <woods49warhorse@hotmail.com>
2017-04-24 13:56:58 -0500

    Update doPlan.c

--- Program6/doPlan.c
+++ Program6/doPlan.c
@@ -10,5 +10,7 @@
 *************************************************************************/
 void doPlan(Graph graph, Plan plan)
 {
-
+  //first do a breadth first traversal I think
+  //within the set level function it just returns the level that course
+  //goes based on
 }
