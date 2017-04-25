58eecb7f235ee9df0e3a417f0c9e0b8c9ee21fe5
William <william@tablepedia.com>
2017-04-25 13:36:18 -0500

    Update doPlan.c

--- Program6/doPlan.c
+++ Program6/doPlan.c
@@ -14,21 +14,21 @@ void doPlan(Graph graph, Plan plan)
   //goes based on
   //read bIncludes
   int i;
-  int iLev = 0;
+  int iLevel = 0;
   for(i = 0; i < MAX_VERTICES; i++)
   {
     if(plan->bIncludeM[i] == TRUE){
-      setLevel(graph, plan, i, &iLev);
+      setLevel(graph, plan, i, 0);
       // need to work on this
-      while (plan->semesterM[iLev][5] != -1){ // if iLev is full increment it.
-        iLev++;      
+      iLevel = graph->vertexM[iVertex].iSemesterLevel;
+      while (plan->semesterM[iLevel][5] != -1){ // if iLev is full increment it.
+        iLevel = graph->vertexM[iVertex].iSemesterLevel + 1;      
       }
         for(i = 0; i < 5; i++) // loof for a space in semester array for the current course
         {
-            if(plan->semesterM[iLev][i] == -1) //found it
-                plan->semesterM[iLev][i] = iVertex;
+            if(plan->semesterM[iLevel][i] == -1) //found it
+                plan->semesterM[iLevel][i] = iVertex;
         }
-     iLev = 0; // set iLev to 0 to pass it to setLev as 0
     }
   }
   //printPlan(graph, plan);//this needs to be added to the print.c file
