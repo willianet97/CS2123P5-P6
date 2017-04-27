4d8416f6ce5d28141f89512801827c6c003c345d
brusnak89 <rhcp002743@gmail.com>
2017-04-27 14:51:30 -0500

    Update deleteCourse.c

--- deleteCourse.c
+++ deleteCourse.c
@@ -29,54 +29,65 @@ if it is in the overflow area.
 ********************************************************/
 void deleteCourse(Graph graph, int iVertex)
 {
-    int i;
-    EdgeNode *e;
-    EdgeNode *e2;
-    EdgeNode *eCurrent;
-    EdgeNode *eCurrent2;
-    EdgeNode *ePrev;
-    EdgeNode *ePrev2;
+    int i;  //local variable to traverse for loop
+    EdgeNode *e;    //local pointer variable used for traversal
+    EdgeNode *e2;   //local pointer variable used for traversal
+    EdgeNode *ePrev;    //local pointer variable used to hold previous node
+    EdgeNode *ePrev2;   //local pointer variable used to hold previous node
+    //traverse through all vertices
     for (i = 0; i < graph->iNumVertices; i++)
     {
+        //initialize ePrev and ePrev2 to NULL
         ePrev = NULL;
         ePrev2 = NULL;
+        //traverse through prereqs
         for (e = graph->vertexM[i].prereqList; e != NULL; e = e->pNextEdge)//the segfault is caused somewhere in here
         {
             if (e->iPrereqVertex == iVertex)
             {
                 if (ePrev == NULL)//if it is at the head of the list
                 {
+                    //set the head of list to the next edge
                     graph->vertexM[i].prereqList = e->pNextEdge;
                     free(e);
                 }
                 else//if it is in the middle
                 {
-                    ePrev->pNextEdge = e->pNextEdge;;//i think this is the issue because its trying to read something null
+                    //set prev next edge to e's next edge
+                    ePrev->pNextEdge = e->pNextEdge;
                     break;
                     
                 }
             }
+            //set ePrev to e
                 ePrev = e;
         }
-         for (e2 = graph->vertexM[i].successorList; e2 != NULL; e2 = e2->pNextEdge)//this works beautifully
+        //traverse through successors
+         for (e2 = graph->vertexM[i].successorList; e2 != NULL; e2 = e2->pNextEdge)
          {
+             //check for head of list
              if (e2->iSuccVertex == iVertex)
              {
+                 //if head of list
                  if (ePrev2 == NULL)
                  {
+                     //set head of list to be the next edge
                     graph->vertexM[i].successorList = e2->pNextEdge;
                     free(e);
                  }
                  else
                  {
-                    eCurrent2 = e2->pNextEdge;
-                    ePrev2->pNextEdge = eCurrent2;
+                     //set ePrev2 next edge to e2's next edge
+                    ePrev2->pNextEdge = e2->pNextEdge;
                 }
              }
+             //set ePrev2 to e2
              ePrev2 = e2;
         }
+        //free e and e2
         free(e);
         free(e2);
     }
+    //set the vertex to false
      graph->vertexM[iVertex].bExists = FALSE;
 }
